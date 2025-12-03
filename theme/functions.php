6d7cdd1082c264b5a59e588e286b471f6c01abd6
Nikolai Dudin <nikolaj.dudin.90@gmail.com>
2025-12-03 13:37:31 +0300

    Add fire effects and animations (orange/red/yellow tones)

--- theme/functions.php
+++ theme/functions.php
@@ -11,7 +11,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Константы темы
-define('TOCHKAGG_THEME_VERSION', '1.0.4'); // Обновляем версию для принудительного обновления файлов
+define('TOCHKAGG_THEME_VERSION', '1.0.5'); // Добавлены огненные эффекты и анимации
 define('TOCHKAGG_THEME_PATH', get_template_directory());
 define('TOCHKAGG_THEME_URI', get_template_directory_uri());
 
