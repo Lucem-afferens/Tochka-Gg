26ea5841bbc168053a98f6b5ea329396d94956e8
Nikolai Dudin <nikolaj.dudin.90@gmail.com>
2025-12-07 14:53:01 +0300

    Убрана кнопка с телефоном из десктопного меню в шапке

--- theme/header.php
+++ theme/header.php
@@ -73,22 +73,6 @@ if (!defined('ABSPATH')) {
             
             <div class="tgg-header__actions">
                 <?php
-                // Получаем телефон из ACF
-                $phone = function_exists('get_field') ? get_field('phone_main', 'option') : false;
-                $phone_display = $phone ?: '8 (992) 222-62-72';
-                $phone_clean = preg_replace('/[^0-9+]/', '', $phone_display);
-                ?>
-                
-                <?php if ($phone_display) : ?>
-                <a href="tel:<?php echo esc_attr($phone_clean); ?>" class="tgg-header__phone" aria-label="Позвонить">
-                    <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
-                        <path d="M22 16.92v3a2 2 0 01-2.18 2 19.79 19.79 0 01-8.63-3.07 19.5 19.5 0 01-6-6 19.79 19.79 0 01-3.07-8.67A2 2 0 014.11 2h3a2 2 0 012 1.72 12.84 12.84 0 00.7 2.81 2 2 0 01-.45 2.11L8.09 9.91a16 16 0 006 6l1.27-1.27a2 2 0 012.11-.45 12.84 12.84 0 002.81.7A2 2 0 0122 16.92z"/>
-                    </svg>
-                    <span class="tgg-header__phone-text"><?php echo esc_html($phone_display); ?></span>
-                </a>
-                <?php endif; ?>
-                
-                <?php
                 // Кнопка бронирования
                 $booking_text = function_exists('get_field') ? get_field('booking_button_text', 'option') : 'Забронировать место';
                 $booking_link = function_exists('get_field') ? get_field('booking_link', 'option') : null;
