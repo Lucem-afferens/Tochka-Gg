ce69467715f1ebadf22039982c7b59caa29ae9ca
Nikolai Dudin <nikolaj.dudin.90@gmail.com>
2025-12-07 18:17:39 +0300

    Добавлена регистрация ACF Options Page через код для ACF Free

--- theme/functions.php
+++ theme/functions.php
@@ -22,5 +22,6 @@ require_once TOCHKAGG_THEME_PATH . '/inc/custom-post-types.php';
 require_once TOCHKAGG_THEME_PATH . '/inc/theme-functions.php';
 require_once TOCHKAGG_THEME_PATH . '/inc/theme-security.php';
 require_once TOCHKAGG_THEME_PATH . '/inc/theme-helpers.php';
+require_once TOCHKAGG_THEME_PATH . '/inc/acf-options.php';
 
 
