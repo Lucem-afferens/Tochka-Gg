2d222439436e44e870e6dd02e979f954672a6acf
Nikolai Dudin <nikolaj.dudin.90@gmail.com>
2025-12-07 14:59:39 +0300

    Заменен основной шрифт на Orbitron для всего проекта, добавлена загрузка из Google Fonts

--- theme/header.php
+++ theme/header.php
@@ -15,6 +15,10 @@ if (!defined('ABSPATH')) {
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <!-- Google Fonts - Orbitron -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;500;600;700;800;900&display=swap" rel="stylesheet">
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
