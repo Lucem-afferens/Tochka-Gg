3b08c8235eb11862cc21c725fe2a7dee466baca1
Nikolai Dudin <nikolaj.dudin.90@gmail.com>
2026-01-01 20:48:59 +0300

    [UX] Принудительное обновление версии стилей - удалены hover эффекты с advantages
    - Увеличена версия темы до 2.2.1 для принудительного обновления кеша
    - Пересобраны все стили
    - Убеждены что hover эффекты удалены из advantages__item

--- theme/functions.php
+++ theme/functions.php
@@ -11,7 +11,7 @@ if (!defined('ABSPATH')) {
 }
 
 // Константы темы
-define('TOCHKAGG_THEME_VERSION', '2.2.0'); // Полная переработка страницы бара, исправление проблем с аккордеоном
+define('TOCHKAGG_THEME_VERSION', '2.2.1'); // Удалены все hover эффекты с некликабельных элементов
 define('TOCHKAGG_THEME_PATH', get_template_directory());
 define('TOCHKAGG_THEME_URI', get_template_directory_uri());
 
